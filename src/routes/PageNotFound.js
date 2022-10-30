b85d25f242e32f7f6c3aefe9eeb3ac2dd54ad878
Camilla Devasini <camilladevasini@gmail.com>
2022-10-30 15:56:00 +0100

    style: add style to not-found-route

--- src/routes/PageNotFound.js
+++ src/routes/PageNotFound.js
@@ -1,16 +1,9 @@
-import pancakeSwapLogo from "../assets/images/pancakeswap-logo.svg";
-import { Link } from "react-router-dom";
+import NotFound from "../components/UI/NotFound";
 
 function PageNotFound() {
 
     return (
-        <div>
-            <img src={pancakeSwapLogo}></img>
-            <h2>404</h2>
-            <p>Oops, page not found</p>
-            <Link to="/">Back home</Link>
-
-        </div>
+        <NotFound />
     )
 }
 export default PageNotFound;
