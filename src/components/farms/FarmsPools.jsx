f6ce7ff4d35ea3efc8fec0303dc521ae04da4a16
Parry93 <parrinelloemanuele@gmail.com>
2022-11-09 15:56:38 +0100

    feat:add farms-section

---
+++ src/components/farms/FarmsPools.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style/farmsPools.scss"
+
+const FarmsPools = () => {
+  return (
+    <div className='cont'>
+  <div className="farms_nav">
+      <a href="!#">Farms</a>
+      <a href="!#">Pools</a>
+    </div>
+    </div>
+  )
+}
+
+export default FarmsPools
--- src/routes/Farms.js
+++ src/routes/Farms.js
@@ -1,7 +1,18 @@
+import FarmsPools from "../components/farms/FarmsPools"
+import FarmsHeader from "../components/farms/FarmsHeader"
+import FarmsNav from "../components/farms/FarmsNav"
+import FarmsAccordion from "../components/farms/FarmsAccordion"
 
 function Farms() {
     return (
-        <div>Farms content</div>
+        <div>
+            <FarmsPools />
+            <FarmsHeader />
+            <br />
+            <FarmsNav />
+            <br />
+            <FarmsAccordion />
+            </div>
     )
 }
 export default Farms;
