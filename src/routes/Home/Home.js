f36afb466db036c11fe4d47e9057d849e256acde
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-11-09 17:26:17 +0100

    feat: add start

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -3,6 +3,7 @@ import { Cake } from "./CakeSection/Cake";
 import { Earn } from "./EarnSection/Earn";
 import { Hero } from "./HeroSection/Hero";
 import { Intro } from "./IntroSection/Intro";
+import { Start } from "./StartSection/Start";
 import { Trade } from "./TradeSection/Trade";
 import { Win } from "./WinSection/Win";
 
@@ -18,6 +19,7 @@ function Home() {
         <Earn/>
         <Win/>
         <Cake/>
+        <Start/>
       
         </>
     )
