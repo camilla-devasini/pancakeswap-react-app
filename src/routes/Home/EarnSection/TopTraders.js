3aa75fc7b4ed77f9848635b90268a5fc67e1d73c
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-11-10 14:33:51 +0100

    fix: intro-section

--- src/routes/Home/EarnSection/TopTraders.js
+++ src/routes/Home/EarnSection/TopTraders.js
@@ -1,14 +1,72 @@
+import { useState } from "react";
+
+
+const textContent = [
+  {
+  
+    titleBlack: "Top",
+    titlePurple: "Farms",
+    subtitle1: "XCAD-CAKE",
+    subtitle2: "CO-BUSD LP",
+    subtitle3: "KRS-BUSD LP",
+    subtitle4: "HOOP-BUSD LP",
+    subtitle5: "MGP-BUSD LP",
+    number1: "247.633%",
+    number2: "187.321%",
+    number3: "184.681%",
+    number4: "160.303%",
+    number5: "142.246%",
+    apr:  "APR"
+  },
+
+  {
+    
+    titleBlack: "Top",
+    titlePurple: "Syrup Pools",
+    subtitle1: "Stake CAKE",
+    subtitle2: "Stake CAKE - Earn XCAD",
+    subtitle3: "Stake CAKE - Earn HOOP",
+    subtitle4: "Stake CAKE - Earn MONI",
+    subtitle5: "Stake CAKE - Earn MGP",
+    number1: "Up to 60.042%",
+    number2: "57.088%",
+    number3: "9.396%",
+    number4: "9.107%",
+    number5: "9.102%",
+    apr: "APR"
+  },
+];
+
+
+
+
 export function TopTraders() {
+
+
+  const [index, setIndex] = useState(0);
+
+  const handleSetIndex = () => {
+
+    setIndex(1)
+
+  }
+
+
   return (
     <>
-      <div className="background-content-data">
+    
+      <div className="background-content-data" >
         <div className="container-data">
           <div className="title">
+          <h2><span>{textContent[index].titleBlack}</span> </h2>
             <h2>
-              <span>Top</span> Farms
+            
+               {textContent[index].titlePurple}
+               
             </h2>
+            
 
-            <button className="button-data">
+            <button onClick={handleSetIndex} className="button-data">
               <svg
                 viewBox="0 0 24 25"
                 height="24px"
@@ -24,83 +82,38 @@ export function TopTraders() {
 
           <div className="group">
             <div>
-              <h2>MHUNT-BNB LP</h2>
-              <h3>126.113%</h3>
-              <h4>APR</h4>
+              <h2>{textContent[index].subtitle1}</h2>
+              <h3>{textContent[index].number1}</h3>
+              <h4>{textContent[index].apr}</h4>
             </div>
 
             <div className="group-border">
-              <h2>AOG-CAKE LP</h2>
-              <h3>120.872%</h3>
-              <h4>APR</h4>
+              <h2>{textContent[index].subtitle2}</h2>
+              <h3>{textContent[index].number2}</h3>
+              <h4>{textContent[index].apr}</h4>
             </div>
 
             <div className="group-border">
-              <h2>XCAD-BUSD LP</h2>
-              <h3>120.601%</h3>
-              <h4>APR</h4>
+              <h2>{textContent[index].subtitle3}</h2>
+              <h3>{textContent[index].number3}</h3>
+              <h4>{textContent[index].apr}</h4>
             </div>
 
             <div className="group-border fourth">
-              <h2>SANTOS-BNB LP</h2>
-              <h3>111.807%</h3>
-              <h4>APR</h4>
+              <h2>{textContent[index].subtitle4}</h2>
+              <h3>{textContent[index].number4}</h3>
+              <h4>{textContent[index].apr}</h4>
             </div>
 
             <div className="group-border fifth">
-              <h2>OLE-BUSD LP</h2>
-              <h3>100.063%</h3>
-              <h4>APR</h4>
+              <h2>{textContent[index].subtitle5}</h2>
+              <h3>{textContent[index].number5}</h3>
+              <h4>{textContent[index].apr}</h4>
             </div>
           </div>
         </div>
-
-        {/* <div className="container-data-2">
-
-                <div className="title">
-                        
-                    <h2><span>Top</span> Syrus Pools</h2>
-
-                        <button className="button-data">
-                        <svg viewBox="0 0 24 25" height="24px" width="24px" color="textSubtle" xmlns="http://www.w3.org/2000/svg" className="sc-8a800401-0 hFFRYD"><path d="M16 17.01V11C16 10.45 15.55 10 15 10C14.45 10 14 10.45 14 11V17.01H12.21C11.76 17.01 11.54 17.55 11.86 17.86L14.65 20.64C14.85 20.83 15.16 20.83 15.36 20.64L18.15 17.86C18.47 17.55 18.24 17.01 17.8 17.01H16ZM8.65003 3.35002L5.86003 6.14002C5.54003 6.45002 5.76003 6.99002 6.21003 6.99002H8.00003V13C8.00003 13.55 8.45003 14 9.00003 14C9.55003 14 10 13.55 10 13V6.99002H11.79C12.24 6.99002 12.46 6.45002 12.14 6.14002L9.35003 3.35002C9.16003 3.16002 8.84003 3.16002 8.65003 3.35002Z"></path></svg>
-                        </button>
-                </div>
-
-                <div className="group">
-
-                    <div>
-                    <h2>Stake </h2>
-                    <h3>UP to 63.853%</h3>
-                    <h4>APY</h4>
-                    </div>
-
-                    <div className="group-border">
-                        <h2>Stake CAKE - Earn SFUND</h2>
-                        <h3>24.598%</h3>
-                        <h4>APR</h4>
-                    </div>
-                    
-                    <div className="group-border">
-                        <h2>Stake Cake - Earn Hay</h2>
-                        <h3>24.207%</h3>
-                        <h4>APR</h4>
-                    </div>
-
-                    <div className="group-border fourth">
-                        <h2>Stake Cake - Earn GQ</h2>
-                        <h3>24.029%</h3>
-                        <h4>APR</h4>
-                    </div>
-
-                    <div className="group-border fifth">
-                        <h2>Stake Cake - Earn SHELL</h2>
-                        <h3>23.919%</h3>
-                        <h4>APR</h4>
-                    </div>
-
-                </div>
-                </div> */}
       </div>
+      
     </>
   );
 }
