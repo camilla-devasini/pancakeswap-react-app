49a1adef7f1fbb44ecbe75919082500c983a0e99
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-11-15 19:29:00 +0100

    fix: top-traders

--- src/routes/Home/EarnSection/TopTraders.js
+++ src/routes/Home/EarnSection/TopTraders.js
@@ -81,7 +81,7 @@ export function TopTraders() {
           </div>
 
           <div className="group">
-            <div>
+            <div className="group1">
               <h2>{textContent[index].subtitle1}</h2>
               <h3>{textContent[index].number1}</h3>
               <h4>{textContent[index].apr}</h4>
