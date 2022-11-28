fcf156f2e2b9c28fe441eae4560e1efbf73e0e21
Camilla Devasini <camilladevasini@gmail.com>
2022-11-28 11:49:14 +0100

    fix themeprovider form modal

--- src/components/wallet/Wallet.js
+++ src/components/wallet/Wallet.js
@@ -1,19 +1,18 @@
 import Modal from "../UI/Modal";
 import ConnectYourWallet from "./ConnectYourWallet";
 import { wallets } from "./walletObj";
+import { ThemeProvider } from "../../context/ColorThemeContext";
 
-const Wallet = props => {
-    
-
-    return (
-       <Modal onClose={props.onClose}>
-          <div>
-            <ConnectYourWallet wallets={wallets} />
-          </div>
-        </Modal>
-
-    ) 
-}
+const Wallet = (props) => {
+  return (
+    <ThemeProvider>
+      <Modal onClose={props.onClose}>
+        <div>
+          <ConnectYourWallet wallets={wallets} />
+        </div>
+      </Modal>
+    </ThemeProvider>
+  );
+};
 
 export default Wallet;
-
