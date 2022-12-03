6dfbfca9c229f023248d94de972e2637618ed0cb
Alessandro Ricci <106540146+AlexRicci93@users.noreply.github.com>
2022-12-03 13:03:28 +0100

    fix: svg-path hero-section

--- src/routes/Home/HeroSection/Hero.js
+++ src/routes/Home/HeroSection/Hero.js
@@ -1,3 +1,4 @@
+
 import astronautbunny from "../../../assets/images/homepage/astronaut-bunny.webp";
 // import lunarbunny from "../../../assets/images/homepage/bunny@2x.webp";
 import starL from "../../../assets/images/homepage/star-l@2x.webp";
@@ -5,7 +6,6 @@ import starR from "../../../assets/images/homepage/star-r@2x.webp";
 import starT from "../../../assets/images/homepage/star-top-r@2x.webp";
 import MainButton from "../../../components/UI/MainButton";
 import { PurpleDiv } from "./PurpleDiv";
-import fadeOut from "../../../assets/images/homepage/svgexport-16.svg";
 
 export function Hero() {
   return (
@@ -48,8 +48,70 @@ export function Hero() {
               />
             </div>
           </div>
+        </div>
+        <div className="svg-wrapper">
+          <svg
+            className="dk-svg"
+            viewBox="0 0 1660 339"
+            width="100%"
+            color="text"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z"
+              fill="url(#paint0_linear_dark)"
+            ></path>
+            <defs>
+              <linearGradient
+                id="paint0_linear_dark"
+                x1="830"
+                y1="83.5"
+                x2="830"
+                y2="338.5"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#08060B" stop-opacity="0.2"></stop>
+                <stop
+                  offset="0.545554"
+                  stop-color="#08060B"
+                  stop-opacity="0.5"
+                ></stop>
+                <stop offset="1" stop-color="#08060B"></stop>
+              </linearGradient>
+            </defs>
+          </svg>
 
-          <svg src={fadeOut} className="svg-border" />
+          <svg
+            className="lt-svg"
+            viewBox="0 0 1660 339"
+            class="sc-4ba21b47-0 IIbzK slide-svg-light"
+            width="100%"
+            color="text"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M804 167.023C520.5 167.023 267.5 290.522 0 304.5V339H1660V0.5C1358.83 0.5 1104 167.023 804 167.023Z"
+              fill="url(#paint0_linear_light)"
+            ></path>
+            <defs>
+              <linearGradient
+                id="paint0_linear_light"
+                x1="830"
+                y1="84"
+                x2="830"
+                y2="339"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="white" stop-opacity="0.48"></stop>
+                <stop
+                  offset="0.566389"
+                  stop-color="white"
+                  stop-opacity="0.35"
+                ></stop>
+                <stop offset="1" stop-color="white"></stop>
+              </linearGradient>
+            </defs>
+          </svg>
         </div>
       </div>
     </>
