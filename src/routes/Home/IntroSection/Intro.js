a3b934764f63f14d8dbf1ebc3188ce440e6a3e67
Alessandro Ricci <106540146+AlexRicci93@users.noreply.github.com>
2022-12-05 14:58:28 +0100

    fix: svg-path earn-section

--- src/routes/Home/IntroSection/Intro.js
+++ src/routes/Home/IntroSection/Intro.js
@@ -76,16 +76,18 @@ export function Intro() {
         </div>
       </div>
       <div className="wrapper-svg">
-      <svg 
-        viewBox="0 0 1660 48"
-        preserveAspectRatio="none"
-        color="text"
-        width="20px"
-        xmlns="http://www.w3.org/2000/svg"
-        class="sc-4ba21b47-0 IIbzK"
-      >
-        <path d="M1660 48C1139.02 46.1887 336.256 15.2453 0 0H1660V48Z"></path>
-      </svg>
+        <svg
+          viewBox="0 0 1660 48"
+          preserveAspectRatio="none"
+          color="text"
+          width="20px"
+          xmlns="http://www.w3.org/2000/svg"
+          class="sc-4ba21b47-0 IIbzK"
+        >
+          <path d="M1660 48C1139.02 46.1887 336.256 15.2453 0 0H1660V48Z"></path>
+        </svg>
+
+        
       </div>
     </>
   );
