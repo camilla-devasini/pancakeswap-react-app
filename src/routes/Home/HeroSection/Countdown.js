c1a14de96600ee64c56d1ed0fdddba936eab3dae
Alessandro Ricci <106540146+AlexRicci93@users.noreply.github.com>
2022-12-06 17:02:15 +0100

    fix: counter hero-section

--- src/routes/Home/HeroSection/Countdown.js
+++ src/routes/Home/HeroSection/Countdown.js
@@ -1,41 +1,37 @@
-import { useEffect, useRef, useState } from "react";
+import moment from "moment";
+import { useEffect,  useState } from "react";
 
 export function Countdown() {
   const [seconds, setSeconds] = useState(59);
   const [minutes, setMinutes] = useState(59);
   const [hour, setHour] = useState(24);
 
-  const ref = useRef();
 
-  let timer = ref;
+
+  
 
   useEffect(() => {
-    timer.current = setInterval(() => {
-      setSeconds(seconds - 1);
-
-      if (seconds === 0) {
-        setMinutes(minutes - 1);
-        setSeconds(59);
-      }
-
-      if (minutes === 0) {
-        setHour(hour - 1);
-        setMinutes(59);
-      }
+    window.setInterval(() => {
+      const date = moment("19/12/2022 17:00:00", "DD/MM/YYYY HH:mm:ss");
+      const _diff = date.diff(moment(), "seconds");
+      const _hours = Math.floor(_diff / 60 / 60);
+      const _minutes = Math.floor((_diff - _hours * 60 * 60) / 60);
+      const _seconds = _diff - _hours * 60 * 60 - _minutes * 60;
+      setSeconds(_seconds);
+      setHour(_hours);
+      setMinutes(_minutes);
     }, 1000);
-
-    return () => clearInterval(timer.current);
   });
 
   return (
     <>
       <div>
         <h1 style={{ fontWeight: "600" }}>
-          {hour}
+          {hour.toString().padStart(2, "0")}
           <span>h </span>
-          {minutes}
+          {minutes.toString().padStart(2, "0")}
           <span>m </span>
-          {seconds}
+          {seconds.toString().padStart(2, "0")}
           <span>s </span>
         </h1>
       </div>
