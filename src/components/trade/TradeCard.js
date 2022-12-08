0d9b29080416302c8d3d33417128cc723da30d59
Camilla Devasini <camilladevasini@gmail.com>
2022-12-08 23:00:38 +0100

    refactor: commented code

--- src/components/trade/TradeCard.js
+++ src/components/trade/TradeCard.js
@@ -21,37 +21,37 @@ function TradeCard(props) {
   const [error, setError] = useState(null);
 
   // COMMENTATO PER LIMITARE LE CHIAMATE API (MAX 100/MONTH)
-  useEffect(() => {
-    fetch(
-      `http://api.coinlayer.com/api/live?access_key=9327ae11e2eba3086a7ae3a0887483d9`
-    )
-      .then((response) => {
-        if (response.status !== 200) {
-          setError(new Error("error occurred"));
-        }
-        return response.json();
+  // useEffect(() => {
+  //   fetch(
+  //     `http://api.coinlayer.com/api/live?access_key=9327ae11e2eba3086a7ae3a0887483d9`
+  //   )
+  //     .then((response) => {
+  //       if (response.status !== 200) {
+  //         setError(new Error("error occurred"));
+  //       }
+  //       return response.json();
 
-        //     })
-        //     .then(json => {
-      })
-      .then((json) => {
-        setRates(json.rates);
-        console.log(json.rates);
-        console.log(Object.keys(rates));
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, []);
+  //       //     })
+  //       //     .then(json => {
+  //     })
+  //     .then((json) => {
+  //       setRates(json.rates);
+  //       console.log(json.rates);
+  //       console.log(Object.keys(rates));
+  //     })
+  //     .catch((error) => {
+  //       setError(error);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    if (!!rates) {
-      function init() {
-        handleAmount1Change(1);
-      }
-      init();
-    }
-  }, [rates]);
+  // useEffect(() => {
+  //   if (!!rates) {
+  //     function init() {
+  //       handleAmount1Change(1);
+  //     }
+  //     init();
+  //   }
+  // }, [rates]);
 
   function format(number) {
     return number.toFixed(4);
